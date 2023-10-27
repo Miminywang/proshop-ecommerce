41807b7754f9e5e3018efac7fc60129751f5e21d
Mimi Wang <miny82525@gmail.com>
2023-10-28 03:22:30 +0800

    新增產品控制器和錯誤中介軟體

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import Product from '../models/productModel.js';
 
 router.get('/', asyncHandler (async (req, res) => {
   const products = await Product.find({});
+  throw new Error('Some Error');
   res.json(products);
 }));
 
@@ -13,10 +14,11 @@ router.get('/:id', asyncHandler (async (req, res) => {
 
   if (product) {
     return res.json(product);
-  }
-
-  res.status(404).json({ message: 'Product not found' });
-  
-}));
+  } else {
+    res.status(404);
+    throw new Error('Resource not found');
+  }  
+ })
+);
 
 export default router;
